bb25f769d8d107ce13cdd48126965c79fd678342
beckLic <augustobeck472@gmail.com>
2025-11-11 09:59:05 -0300

    mina movil funcionando en el lugar y cambie bases de J2

--- Visual/main.py
+++ Visual/main.py
@@ -75,10 +75,10 @@ def inicializar_simulacion():
     # (El mapa es 50x50, así que usamos coordenadas opuestas)
     base_jugador_2 = (44, 44) # (x, y) en grilla (esquina inferior derecha)
 
-    jeep_ia_2 = Jeep(id="J-IA-2", jugador_id=2, pos_inicial=(0, 30), posicion_base=base_jugador_2)
-    moto_ia_2 = Moto(id="M-IA-2", jugador_id=2, pos_inicial=(0, 35), posicion_base=base_jugador_2)
-    camion_ia_2 = Camion(id="C-IA-2", jugador_id=2, pos_inicial=(0, 40), posicion_base=base_jugador_2)
-    auto_ia_2 = Auto(id="A-IA-2", jugador_id=2, pos_inicial=(0, 45), posicion_base=base_jugador_2)
+    jeep_ia_2 = Jeep(id="J-IA-2", jugador_id=2, pos_inicial=(0, 30), posicion_base=(0, 30))
+    moto_ia_2 = Moto(id="M-IA-2", jugador_id=2, pos_inicial=(0, 35), posicion_base=(0, 35))
+    camion_ia_2 = Camion(id="C-IA-2", jugador_id=2, pos_inicial=(0, 40), posicion_base=(0, 40))
+    auto_ia_2 = Auto(id="A-IA-2", jugador_id=2, pos_inicial=(0, 45), posicion_base=(0, 45))
 
     # 4. Añadirlos TODOS al grupo de vehículos
     grupo_vehiculos.add(
@@ -123,7 +123,7 @@ run = True
 simulacion_iniciada = False
 inicializar_simulacion()
 while run:
-    reloj.tick(20)#FPS
+    reloj.tick(10)#FPS
     game_time += 1
     ventana.fill(CONSTANTES.COLOR_NEGRO)
     # Dibujamos el fondo del panel de UI
